38ef0f912c198fc9ca6620af2b2a77eba27e0620
AleksandrStupnikov <a-a-stupnikov@yandex-team.ru>
2024-07-03 17:10:04 +0300

    hw3.2

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate(double weight, double height) {
+        int index;
+        index = (int) (weight / Math.pow(height, 2));
+        return (index);
+
+    }
+}
